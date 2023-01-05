0dcbf14814aefbefb1607ed5f4cb72953248afff
Claire <claire.hw.cho@gmail.com>
2023-01-04 20:26:04 -0800

    change syntax of texttype in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,46 @@
 // Text typing effect
-const TextType = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 6000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TextType.prototype.tick = function () {
-    const i = this.loopNum % this.toRotate.length;
-    const fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-    const that = this;
-    const delta = 150 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+class TextType {
+    constructor (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 6000;
+        this.txt = '';
+        this.tick();
         this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
     }
+    tick () {
+        const i = this.loopNum % this.toRotate.length;
+        const fullTxt = this.toRotate[i];
 
-    setTimeout(function () {
-        that.tick();
-    }, delta);
-};
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        const that = this;
+        const delta = 150 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    }
+}
+
 
 window.onload = function () {
     const elements = document.getElementsByClassName('intro_position');
