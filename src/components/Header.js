cb3d1098dd9eb88645a85b22bbe97e339ae186bc
Claire <claire.hw.cho@gmail.com>
2023-04-18 17:15:59 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+    const tbu = () => {
+        alert("Will be updated soon!");
+    };
+
+    return (
+        <header>
+            <div className="logo">
+                <Link to="/" className="link">
+                    <h5>
+                        HYUNG WON <span>CHO</span>
+                    </h5>
+                </Link>
+            </div>
+
+            <nav className="header-nav">
+                <ul className="header-nav-ul">
+                    <li>
+                        <Link to="#about-me" className="link">About me</Link>
+                    </li>
+                    <li>
+                        <Link to="#work" className="link">Work</Link>
+                    </li>
+                    <li>
+                        <Link to="#contact-me" className="link">Contact me</Link>
+                    </li>
+                    <li>
+                        <Link to="#" className="link" onClick={tbu}>Resume</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header >
+    );
+};
+
+export default Header;
