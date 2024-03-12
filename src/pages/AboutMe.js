9d5c3a9fe4bee14b8a4801ef72d0cc6f6725eabb
Claire <claire.hw.cho@gmail.com>
2024-03-12 15:43:02 -0700

    update about me content

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -44,23 +44,23 @@ const AboutMe = (props) => {
 
                 <div className="main-content-section-body-row">
                     <p>Hi! My name is Hyung Won, but you can also call me Claire. I am a full-stack developer who can get a project done from start to finish.</p>
-                    <p>With extensive experience as a Community Program Manager and Conference Project Manager in the hospitality industry, I've demonstrated proficiency in managing concurrent customer-focused programs and projects while ensuring alignment with organizational goals. Combining my background in hospitality with robust coding skills acquired through coding bootcamps, I am ready to thrive as a software engineer, bringing a distinct blend of customer-centricity and technical proficiency to make impactful contributions in this dynamic field of software engineering!</p>
+                    <p>With extensive experience as a Community Program Manager and Conference Project Manager in the hospitality industry, I successfully led customer-centric initiatives. Combining my background in hospitality with robust coding skills acquired through coding bootcamps, I am ready to thrive as a full-stack software engineer, bringing a distinct blend of customer-centricity and technical proficiency to make impactful contributions in this dynamic field of software engineering!</p>
                     <p>Here are the technologies that I can use:</p>
                     <ul className="tech-ul">
                         <li>
                             Languages:
-                            <span> Python, Java, JavaScript (ES6, Node.js), HTML5, CSS, SQL</span>
+                            <span> JavaScript (ES6, Node.js), Python, Java, HTML5, CSS, SQL, GraphQL</span>
                         </li>
                         <li>
                             Frameworks/Libraries:
-                            <span> React.js, Express.js, jQuery, Flask, Jinja2, Spring Boot, Bootstrap, Materialize, Material UI</span>
+                            <span> React.js, Express.js, jQuery, Flask, Django, Jinja2, Spring Boot, Bootstrap, Materialize, Material UI</span>
                         </li>
                         <li>
                             Databases:
                             <span> MySQL, MongoDB</span>
                         </li>
                         <li>Tools:
-                            <span> Visual Studio Code, AJAX, Spring Tool Suite, MySQL Workbench, Postman, Insomnia, AWS(EC2), Git, Github, Adobe Premiere Pro, Adobe Photoshop, Adobe Illustrator</span>
+                            <span> Visual Studio Code, AJAX, Spring Tool Suite, MySQL Workbench, Postman, Insomnia, AWS(EC2), Git, GitHub, Adobe Premiere Pro, Adobe Photoshop, Adobe Illustrator, Microsoft Office Suite</span>
                         </li>
                     </ul>
                     <p>Check my <Link to="/projects" className="link"><span className="about-me-link">projects</span></Link> and <Link to="https://docs.google.com/document/d/1Ky77JxGOYP1ZG8Q88lR58itz5aNDa3eJSByYPFZ4yE0/edit" target="_blank" className="link"><span className="about-me-link">resume</span></Link> to learn more about me!</p>
